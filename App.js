384e8b59801a5ad8a5954ebee6be538e5359be79
Aniruddh <ojhaa94@gmail.com>
2021-04-22 23:19:16 +0100

    connects dashboard file to main app

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Dashboard from './components/dashboard.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+			<Dashboard />
     </View>
   );
 }
