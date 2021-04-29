4bba88ef3d6d35362f8e6adb9a5f20ea72336faf
Aniruddh <ojhaa94@gmail.com>
2021-04-29 11:22:05 +0100

    adds navigation pages

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Dashboard from './components/dashboard.js';
+import Swiper from 'react-native-swiper'
+import randomcolor from 'randomcolor'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-			<Dashboard />
-    </View>
-  );
+// const {
+// 	// View,
+// 	Text,
+// 	StyleSheet
+// } = React
+
+
+class TitleText extends React.Component {
+  render() {
+    return (
+      <Text style={{ fontSize: 48, color: 'white' }}>
+        {this.props.label}
+      </Text>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Home extends React.Component {
+
+	viewStyle() {
+		return {
+			flex: 1,
+			backgroundColor: randomcolor(),
+			justifyContent: 'center',
+			alignItems: 'center',
+		}
+	}
+
+	render() {
+		return (
+			<Swiper
+				loop={false}
+				showsPagination={true}
+				index={1}>
+				{/* <View style={this.viewStyle()}>
+					<TitleText label="Waddup" />
+				</View> */}
+				<Dashboard />
+				{/* <Swiper
+					horizontal={false}
+					loop={false}
+					showsPagination={false}
+					index={1}>
+					<View style={this.viewStyle()}>
+						<TitleText label="Top" />
+					</View>
+					<View style={this.viewStyle()}>
+						<TitleText label="Home" />
+					</View>
+					<View style={this.viewStyle()}>
+						<TitleText label="Bottom" />
+					</View>
+				</Swiper> */}
+				<View style={this.viewStyle()}>
+					<TitleText label="Buds" /> 
+				</View>
+			</Swiper>
+
+			
+		)
+	}
+}
+
+
+
+export default Home
--- components/dashboard.js
+++ components/dashboard.js
@@ -11,3 +11,32 @@ export default class Dashboard extends Component {
 	}
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 10,
+  },
+  top: {
+    flex: 0.2,
+    backgroundColor: "grey",
+    borderWidth: 5,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  middle: {
+    flex: 1,
+    backgroundColor: "beige",
+    borderWidth: 5,
+  },
+  bottom: {
+    flex: 0.3,
+    backgroundColor: "pink",
+    borderWidth: 5,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+});
+
